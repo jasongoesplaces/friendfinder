bee890331ae6b0d7416947f5ec5c766be90c93dc
Jason <jasongoesplaces@gmail.com>
2018-05-12 02:03:28 -0400

    file updates

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friends = [
+    {
+        name: "Bob",
+        photo: "http://mblogthumb4.phinf.naver.net/MjAxNzAxMTFfMTIg/MDAxNDg0MDk2ODkxMDc3.ERC7jGOMp3Z9t4FI4nRWuLTL-8OuG1R4PRqZ-lcHcPAg.tDTjnph3KwppJJXXt65X3jc_Ck9MmYn6YbjUESs58-4g.JPEG.gwemool/photo.jpeg?type=w800",
+        scores: [
+            "3",
+            "3",
+            "2",
+            "2",
+            "4",
+            "4",
+            "2",
+            "2",
+            "3",
+            "3"
+        ]
+    },
+    {
+        name: "Mr. Rodgers",
+        photo: "https://daily.jstor.org/wp-content/uploads/2018/03/mister_rogers_feature_2_1050x700.jpg",
+        scores: [
+            "2",
+            "2",
+            "2",
+            "3",
+            "4",
+            "4",
+            "3",
+            "2",
+            "2",
+            "2"
+        ]
+    },
+    {
+        name: "Steve",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Steve_Jobs_Headshot_2010-CROP.jpg/1200px-Steve_Jobs_Headshot_2010-CROP.jpg",
+        scores: [
+            "4",
+            "4",
+            "4",
+            "2",
+            "5",
+            "1",
+            "2",
+            "4",
+            "4",
+            "4"
+        ]
+    }
+]
+
+module.exports = friends
